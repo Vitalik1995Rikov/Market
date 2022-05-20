a060216723613903e2a0fdd43a9393009a11c86e
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-20 21:49:56 +0300

    clean projext

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,5 @@
-import Header from "./components/Header/Header";
-
-
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+  return <div className="App">Clean</div>;
 }
 
 export default App;
