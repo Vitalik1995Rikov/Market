5bd76515cca5d2aedf03de274ec6be73553d2907
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-23 21:05:18 +0300

    get error

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Header from './components/Header';
+import { Provider } from 'react-redux';
+import { store } from './redux';
 
 function App() {
   return (
-    <Router>
-      <div className="bg-[#4d4d4d] text-[#FFFFFF] p-[20px]">
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="bg-[#4d4d4d] text-[#FFFFFF] p-[20px]">
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
--- my-app/src/components/GameBuy.js
+++ my-app/src/components/GameBuy.js
@@ -1,10 +1,19 @@
 import Button from './Button';
+import { useDispatch } from 'react-redux';
+import { setItemsInCart } from '../redux/cart/reducer';
 
 function GameBuy({ game }) {
+  const dispatch = useDispatch();
+
+  const clickButton = (e) => {
+    // e.stopPropagation();
+    dispatch(setItemsInCart(game));
+  };
+
   return (
     <div className="flex justify-end">
       <p className="flex items-center mr-3">{game.price} руб.</p>
-      <Button />
+      <Button onClick={clickButton} />
     </div>
   );
 }
