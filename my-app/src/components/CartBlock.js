68222bc7f56a3e289e1c6b2740b6e89dfc996def
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-24 21:15:49 +0300

    added counter for prices

--- my-app/src/components/CartBlock.js
+++ my-app/src/components/CartBlock.js
@@ -1,10 +1,13 @@
 import { BsCart } from 'react-icons/bs';
+import { useSelector } from 'react-redux';
 
 function CartBlock() {
+  const games = useSelector((state) => state.cart.itemsInCart);
+  const sumOfPrises = games.reduce((accum, game) => (accum += game.price), 0);
   return (
     <div className="flex">
       <BsCart className="h-[30px]" />
-      <p className="text-xl">2333 p.</p>
+      <p className="text-xl">{sumOfPrises}</p>
     </div>
   );
 }
