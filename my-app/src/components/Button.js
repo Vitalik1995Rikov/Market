dec25293a8e1806c3b603047cb6b37c4cab9ffa0
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-24 21:30:55 +0300

    added some logic for change name of button

--- my-app/src/components/Button.js
+++ my-app/src/components/Button.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setItemInCart } from '../redux/cart/reducer';
 
 function Button({ game }) {
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const isIteminCart = items.some((item) => item.id === game.id);
+
   const dispatch = useDispatch();
   function clickButton(e) {
     e.stopPropagation();
@@ -12,7 +15,7 @@ function Button({ game }) {
     <button
       onClick={clickButton}
       className="bg-[#101010] h-8 w-32 rounded-md flex justify-center items-center">
-      В корзину
+      {isIteminCart ? 'Убрать' : 'В корзину'}
     </button>
   );
 }
