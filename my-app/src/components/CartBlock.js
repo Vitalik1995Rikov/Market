e8e4248673284d3551e31a87de932da266becdff
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-26 20:50:03 +0300

    added logic for visible/hiden CartMenu component

--- my-app/src/components/CartBlock.js
+++ my-app/src/components/CartBlock.js
@@ -1,17 +1,17 @@
+import { useState } from 'react';
 import { BsCart } from 'react-icons/bs';
 import { useSelector } from 'react-redux';
 import CartMenu from './CartMenu';
 
 function CartBlock() {
+  const [isCartMenuVisible, setIsCartMenuVisible] = useState(false);
   const games = useSelector((state) => state.cart.itemsInCart);
   const sumOfPrises = games.reduce((accum, game) => (accum += game.price), 0);
   return (
     <div className="flex">
-      <BsCart className="h-[30px]" />
+      <BsCart className="h-[30px]" onClick={() => setIsCartMenuVisible(!isCartMenuVisible)} />
       {sumOfPrises ? <p className="text-xl">{sumOfPrises} руб.</p> : null}
-      <div>
-        <CartMenu games={games} sumOfPrices={sumOfPrises} />
-      </div>
+      <div>{isCartMenuVisible && <CartMenu games={games} sumOfPrices={sumOfPrises} />}</div>
     </div>
   );
 }
