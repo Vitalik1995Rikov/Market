118d9a59759c3eed645bd7fec9e262254cb2fd3f
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-27 23:57:56 +0300

    added dispatch and history

--- my-app/src/components/GameItem.js
+++ my-app/src/components/GameItem.js
@@ -2,10 +2,17 @@ import GameImg from './GameImg';
 import GameBuy from './GameBuy';
 import Genres from './Genres';
 import React from 'react';
+import useHistory from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 function GameItem({ game }) {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    return;
+  };
   return (
-    <div className="bg-[#1F1F1F] rounded-xl m-[20px]">
+    <div className="bg-[#1F1F1F] rounded-xl m-[20px]" onClick={handleClick}>
       <GameImg img={game.image} />
       <div className="p-[10px]">
         <h1 className="text-xl">{game.title}</h1>
