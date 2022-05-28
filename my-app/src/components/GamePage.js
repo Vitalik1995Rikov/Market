23759bc772c47d150195a325e94eadbad7fc6593
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-28 22:32:57 +0300

    some changes in GamePage

--- my-app/src/components/GamePage.js
+++ my-app/src/components/GamePage.js
@@ -1,5 +1,18 @@
+// import GameImg from './GameImg';
+// import Genres from './Genres';
+// import GameBuy from './GameBuy';
+
 function GamePage() {
-  return <div>GamePage</div>;
+  return (
+    <div className="bg-[#1F1F1F] rounded-xl m-[20px]">
+      <div>img</div>
+      <div className="p-[10px]">
+        <h1 className="text-xl">title</h1>
+        <div className="flex flex-row"></div>
+        <div>GameBuy</div>
+      </div>
+    </div>
+  );
 }
 
 export default GamePage;
