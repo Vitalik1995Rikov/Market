cfa3d3b7cdf91ff81b8987aabbb3c65430a05472
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-29 18:46:30 +0300

    added logic for GamePage component

--- my-app/src/components/GamePage.js
+++ my-app/src/components/GamePage.js
@@ -1,15 +1,20 @@
-// import GameImg from './GameImg';
-// import Genres from './Genres';
-// import GameBuy from './GameBuy';
+import GameImg from './GameImg';
+import Genres from './Genres';
+import GameBuy from './GameBuy';
+import { useSelector } from 'react-redux';
 
 function GamePage() {
+  const game = useSelector((state) => state.games.currentGame);
+  if (!game) return null;
   return (
     <div className="bg-[#1F1F1F] rounded-xl m-[20px]">
       <div>img</div>
       <div className="p-[10px]">
         <h1 className="text-xl">title</h1>
-        <div className="flex flex-row"></div>
-        <div>GameBuy</div>
+        <iframe title="video" src={game.video} frameBorder="0"></iframe>
+        <GameImg img={game.image} />
+        <Genres genre={game.genres} />
+        <GameBuy game={game} />
       </div>
     </div>
   );
