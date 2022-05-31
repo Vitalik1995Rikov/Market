d73df4662a7aa5f2ad6d5d45c21887cd91b8f626
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-31 14:01:37 +0300

    added new component OrderItem

--- my-app/src/components/OrderPage.js
+++ my-app/src/components/OrderPage.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import OrderItem from './OrderItem';
 
 function OrderPage() {
-  return <div>Order</div>;
+  const items = useSelector((state) => state.cart.itemsInCart);
+  return (
+    <div>
+      <div>
+        {items.map((item) => (
+          <OrderItem />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default OrderPage;
